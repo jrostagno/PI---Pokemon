732351f21f4c8e2f4dcf9b56596e7378730d8460
Javier Rostagno <88237623+jrostagno@users.noreply.github.com>
2021-11-21 23:09:57 -0300

    commit funciones

---
+++ api/src/functions/functions.js
@@ -0,0 +1,153 @@
+const{Pokemon,Tipo}= require('../db.js')
+
+const axios = require('axios');
+
+
+
+//OBTENGO UN ARRAY CON LOS 40 POKEMONES DE LA API DONDE TENGO SU NOMBRE Y LA URL CON SUS PROPIEDADES.
+const allPokemons = async()=>{
+
+    const infoApi = await axios.get("https://pokeapi.co/api/v2/pokemon");
+
+    const infoApiPokemons = infoApi.data.results;
+ 
+    const infoApi1 = await axios.get(infoApi.data.next);
+ 
+    const infoApiPokemons1 = infoApi1.data.results;
+ 
+    const allPokeInfo = await infoApiPokemons.concat(infoApiPokemons1);
+ 
+    const infoBD = await Pokemon.findAll({
+      includes:{
+          model: Tipo,
+          attributes:['name'],
+          through:{
+              attributes:[],
+          }
+ 
+      }
+    });
+ 
+    const baseDatos = [...infoBD, ...allPokeInfo];
+  console.log(baseDatos)
+ // return baseDatos;
+
+}
+
+
+// FUNCION NOS DA LOS DETALLES DE LOS POKEMONS PARA LA RUTA PRINCIPAL
+
+const getInfo = async () => {
+   
+    const baseDatos= await allPokemons()
+
+   let infoDetail=[];
+
+  for(let i=0; i<baseDatos.length; i++){
+
+   if(baseDatos[i].url){
+
+        const poke = await axios.get(baseDatos[i].url)
+        const datos= poke.data
+      
+        infoDetail.push({
+            name: datos.name,
+            img: datos.sprites.front_default,
+            types: datos.types.map(el=>el.type.name),
+            strength: datos.stats[1].base_stat
+        })
+
+    }else{
+
+        infoDetail.push({
+
+          name: baseDatos[i].name,
+          img: "https://media.giphy.com/media/DRfu7BT8ZK1uo/giphy.gif",
+          //types:baseDatos[i].Tipo.map(el=>)
+          strength:baseDatos[i].strength
+        })
+    }
+   }
+  // console.log(infoDetail)
+   return infoDetail;
+
+ };      
+
+
+ // FUNCION NOS DA EL DETALLE DE UN POKEMON EN PARTICULAR  POR ID
+
+ const pokemonDetail = async (idPokemon) => {
+  
+
+
+   try{
+        const pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`)
+
+        const  data= pokemon.data;
+    
+        const pokeID= {
+            id: data.id,
+            name: data.name,
+            hp: data.stats[0].base_stat,
+            strength: data.stats[1].base_stat,
+            defense: data.stats[2].base_stat,
+            speed: data.stats[5].base_stat,
+            height: data.height,
+            weight: data.weight,
+            image: data.sprites.front_default,
+            types: data.types.map((el) => el.type.name),
+          };
+    
+        return pokeID
+        // console.log(pokeID)
+    
+    }  catch(err) {};
+
+        try{
+            const pokemonBaseDato= await Pokemon.findByPk(idPokemon,{include: Tipo});
+
+            const pokeDataBase= {
+
+                id: pokeDataBase.id,
+                name: pokeDataBase.name,
+                hp: pokeDataBase.hp,
+                strength:pokeDataBase.strength,
+                defense: pokeDataBase.defense,
+                speed: pokeDataBase.speed,
+                height: pokeDataBase.height,
+                weight: pokeDataBase.weight,
+                image: "https://media.giphy.com/media/DRfu7BT8ZK1uo/giphy.gif",
+                type: pokeDataBase.Tipo.map(el=>el.name)
+               
+            }
+
+            return pokeDataBase;
+
+        }catch(err){
+            return {};
+        }
+      
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports={
+    allPokemons,
+    getInfo,
+    pokemonDetail,
+
+ };
+
+
+
+
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,51 @@
 const { Router } = require('express');
+const{Pokemon,Tipo}= require('../db.js')
+const {allPokemons,getInfo, pokemonDetail } = require('../functions/functions.js')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const axios = require('axios');
 
 
 const router = Router();
-
+pokemonDetail(2);
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+  
+
+
+
+//     const allFullinfo=  allPokeInfo.map(async (el)=>{
+
+//         let pokeDetails= await axios.get(el.url);
+//                 let details = pokeDetails.data;
+
+//                 return {
+//                     id:details.id,
+//                     name:details.name,
+//                     hp:details.stats[0].base_stat,
+//                     strength:details.stats[1].base_stat,
+//                     defense: details.stats[2].base_stat,
+//                     speed: details.stats[5].base_stat,
+//                     height: details.height,
+//                     weight: details.weight,
+//                     image: details.sprites.front_default,
+//                     types: details.types.map(el=>el.type.name)
+
+//                 }
+                             
+//     })
+
+
+    
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router;
